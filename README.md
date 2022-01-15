9d67afb362fa6ac1b840baa23ec6c7245b47406a
Bennett Marra <bmarra@email.sc.edu>
2022-01-15 15:38:08 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# AdventOfCode
+ My solutions for the 2021 Advent of Code
---
+++ main.py
@@ -0,0 +1,28 @@
+# Bennett Marra 2022
+
+# Read input from file
+import sys
+
+file = open('input.txt','r')
+newLines = file.read()
+stringContents = newLines.splitlines()
+#print(stringContents)
+
+# convert strings into ints
+intContents = []
+for line in stringContents:
+    intLine = int(line)
+    intContents.append(intLine)
+
+#print(intContents)
+
+# Setup complete, now for the problem
+timesIncreased = 0
+oldVal = sys.maxsize # makes initial oldVal biggest int possible
+
+for newVal in intContents:
+    if oldVal < newVal:
+        timesIncreased += 1
+    oldVal = newVal
+
+print(timesIncreased)
