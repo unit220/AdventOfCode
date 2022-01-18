22378aaf6c8f11f4fa0eaedd83e0d4e908e937e2
Bennett Marra <bmarra@email.sc.edu>
2022-01-18 14:11:26 -0500

    Finish day4 using online solution
    taken from this page https://www.youtube.com/watch?v=zAjliaaIjIc plan to return later to solve this with a 2d array

--- Day4/Day4.py
+++ Day4/Day4.py
@@ -1,6 +1,64 @@
-# Bennett Marra 2022
+from __future__ import annotations
+from typing import NamedTuple
 
 # Read input from file
 file = open('./input.txt', 'r')
 lines = file.read()
+file.close
 
+
+class Board(NamedTuple):
+    left: set[int]
+    ints: list[int]
+
+    # see if someone won
+    @property
+    def has_won(self) -> bool:
+        for i in range(5):
+            for j in range(5):
+                if self.ints[i * 5 + j] in self.left:
+                    break
+            else:
+                return True
+
+            for j in range(5):
+                if self.ints[i + 5 * j] in self.left:
+                    break
+            else:
+                return True
+        else:
+            return False
+
+    @classmethod
+    def parse(cls, board: str) -> Board:
+        ints = [int(s) for s in board.split()]
+        left = set(ints)
+        return cls(left, ints)
+
+
+# parse ints/boards
+first, *rest = lines.split('\n\n')
+boards = [Board.parse(board) for board in rest]
+ints = [int(s) for s in first.split(',')]
+
+
+# call the nums and remove if hit
+def get_number() -> int:
+    for number in ints:
+        for board in boards:
+            board.left.discard(number)
+
+        for board in boards:
+            if board.has_won:
+                return sum(board.left) * number
+
+    raise AssertionError('unreachable')
+
+
+print(get_number())
+
+# Store called numbers
+# calledNumbersStr = lines.split("\n")
+# print(calledNumbersStr)
+# calledNumbers = [calledNumbersStr.split(",")]
+# print(calledNumbers)
